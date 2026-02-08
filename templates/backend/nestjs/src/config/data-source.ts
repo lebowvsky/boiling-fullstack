b456caba69be5ef0e8ebcb73040ceb19a03f23f5
Brice <lebowvsky@gmail.com>
2026-02-08 02:00:42 +0100

    feat: :memo: add production db migration and update readme

---
+++ templates/backend/nestjs/src/config/data-source.ts
@@ -0,0 +1,9 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, '..', 'migrations', '*.{ts,js}')],
+});
